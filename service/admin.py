2fb843a0d213b81798adb00bc9d0f0f9975ea3a5
dis6265 <dishapatil7778@gmail.com>
2024-04-07 14:46:33 +0530

    new field added in databse

--- service/admin.py
+++ service/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from service.models import Service
 
 class ServiceAdmin(admin.ModelAdmin):
-    list_display=('service_icon','service_title','service_des','service_img','service_img1','service_img2','service_img3','service_img4')
+    list_display=('service_name','service_img','service_img1','service_img2','service_img3','service_type','service_avail','service_des') 
+
+    # 'service_creation_data','service_update_data'
 
 
 admin.site.register(Service,ServiceAdmin)
---
+++ views.py
@@ -0,0 +1,137 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+import mysql.connector as sql
+from django.db import IntegrityError
+
+def homepage(request):
+    return render(request,"index2.html")
+
+''' def login(request):
+#     return render(request,"internalfiles/login.html")
+
+# def registration(request):
+#     return render(request,"internalfiles/Registration.html")'''
+
+def service(request):
+    return render(request,"internalfiles/services.html")
+
+def aboutUs(request):
+    # return HttpResponse("welcome to mysite")
+    return render(request,"internalfiles/about-us.html")
+
+def iss(request):
+    # return redirect('is')
+    return render(request,"internalfiles/is.html")
+
+def rooms(request):
+    return render(request,"internalfiles/rooms.html")
+
+
+def Villas(request):
+    return render(request,"internalfiles/Villas.html")
+
+def Building(request):
+    return render(request,"internalfiles/Building.html")
+
+def House(request):
+    return render(request,"internalfiles/House.html")
+
+def listyourproprty(request):
+    #return HttpResponse("List your property")
+    return render(request,"internalfiles/lyp.html")
+
+def takeonrent(request):
+    #return HttpResponse("Takes on Rent")
+    return render(request,"internalfiles/tor.html")
+
+
+
+fn=''
+ln=''
+em=''
+pwd=''
+mn=''
+ct=''
+lem=''
+lpwd=''
+def userform(request):    
+    global fn,ln,em,pwd,mn,ct
+    if request.method=='POST':
+        obj=sql.connect(host="localhost",user="root",password="dishapatil",database="mysite")
+        cursor=obj.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="FN":
+                fn=value
+            if key=="LN":
+                ln=value
+            if key=="mail":
+                em=value
+            if key=="pass":
+                pwd=value
+            if key=="MN":
+                mn=value
+            if key=="City":
+                ct=value
+
+        try:
+            c="insert into users Values('{}','{}','{}','{}','{}','{}')".format(fn,ln,em,pwd,mn,ct)
+            cursor.execute(c)
+            obj.commit()
+        except :
+            return render(request, 'internalfiles/Registration.html',{'form':d,'error_massage':'This email is already registered. please login'})
+
+        # try:
+        #     c="insert into users Values('{}','{}','{}','{}','{}','{}')".format(fn,ln,em,pwd,mn,ct)
+        #     cursor.execute(c)
+        #     obj.commit()
+
+        # except:
+            
+        return redirect('login')
+    return render(request,"internalfiles/Registration.html")
+
+
+def loginform(request):
+    global lem,lpwd
+    if request.method=='POST':        
+        obj=sql.connect(host="localhost",user="root",password="dishapatil",database="mysite")
+        cursor=obj.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="UserName":
+                lem=value
+            if key=="Password":
+                lpwd=value
+        c="select * from users where email='{}' and password='{}'".format(lem, lpwd)    
+        cursor.execute(c)
+        t=tuple(cursor.fetchall())
+        #t=(cursor.fetchall()) 
+        print("Empty {}".format(t))
+        if t==():
+             print("Empty {}".format(t))
+             return render(request, 'internalfiles/login.html',{'form':d,'error_massage':'invalid email or password. Please re-enter'})
+    
+        else:
+            print("Empty {}".format(t))
+            return redirect('service')
+            # return render(request,"internalfiles/services.html")
+    return render(request,"internalfiles/login.html",{'output':em})
+
+'''def homepage(request):
+    data={
+        "title":"Home Page",
+        "bdata" : "welcome to mysiye",
+        "clist" :['C','C++','Python'],
+        "list" :[],#10,20,30,40,5,15
+        'sd':[
+            {'name':'disha','phone':9876543321},
+            {'name':'patil','phone':9827318283}
+        ]
+    }
+    
+    return render(request,"index2.html",data)'''
+
+
+
+    
